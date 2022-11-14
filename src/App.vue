b995d482f3e3b7b7d6da4b6e77a6c54473303c1e
sensiz <dev.federicosensi@gmail.com>
2022-11-14 15:42:38 +0100

    header complete

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,48 @@
 <script>
+import axios from 'axios'
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import { store } from './store.js'
+
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppMain,
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    callApi(url) {
+      axios.get(url)
+        .then(response => {
+          console.log(response)
+          this.store.characters = response.data.results
+          this.store.info = response.data.info
+        })
+        .catch(err => {
+          console.error(err.message)
+          this.store.error = err.message
+        })
+    }
+  },
+  mounted() {
+    this.callApi(this.store.API_URL)
+    console.log(this.store)
+  }
+}
+
+
 </script>
 
 <template>
+
+  <AppHeader />
+  <AppMain />
 
 </template>
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+    name: 'AppHeader'
+}
+
+</script>
+
+<template>
+
+    <header>
+        <img src="../../public/Breaking_Bad_logo.svg" alt="">
+        <h1>Breaking Bad Characters</h1>
+    </header>
+
+</template>
+
+<style>
+
+</style>
